b311c1ec879d7a2b43a2465afc0f97e2e4c17ab4
AndiKod <andrei@andikod.fr>
2022-10-23 14:34:10 +0200

    reset

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
 
-
-// get an item at key "leo" from collection animals
-let item = await animals.get("leo")
-
 app.all('/', (req, res) => {
 
     console.log("Just got a request!")
