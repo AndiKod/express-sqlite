52e9f242e2b8367d5c09e60851a874b08c98f881
AndiKod <andrei@andikod.fr>
2022-10-23 15:35:55 +0200

    un reliable platform

--- index.js
+++ index.js
@@ -10,28 +10,10 @@ const app = express()
 
 app.all('/', (req, res) => {
 
-    const CyclicDB = require('cyclic-dynamodb')
-    const db = CyclicDB('doubtful-dove-gownCyclicDB') 
 
-    const run = async function(){
 
-    let animals = db.collection('animals')
 
-    // create an item in collection with key "leo"
-    let leo = await animals.set('leo', {
-        type:'cat',
-        color:'orange'
-    })    
-
-       
-
-    let foo = " BAR "
-    console.log("vraiment? aucun log?")
-    res.send('De la merde' + foo)
-}
-run()
-
-        
+    res.send('De la merde')
 
 
 
