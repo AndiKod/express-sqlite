8431fbcab47c28aa754983f3c1d6b2affb368266
AndiKod <andrei@andikod.fr>
2022-10-23 13:47:14 +0200

    foo

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo, yo, yo!')
+    res.send('Yo, yo, yo. Im a private Repo!')
 })
 app.listen(process.env.PORT || 3000)
