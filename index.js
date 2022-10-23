af4960246ad2d3420620ca272bf0a331bea1278a
AndiKod <andrei@andikod.fr>
2022-10-23 15:04:39 +0200

    bof

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-const CyclicDb = require("cyclic-dynamodb")
-const db = CyclicDb("brainy-shawl-elkCyclicDB")
+
 
 
 
@@ -12,27 +11,12 @@ console.log("vraiment? aucun log?")
 app.all('/', (req, res) => {
 
 
-    const run = async function(){
-    
-    let animals = db.collection('animals')
 
-        // create an item in collection with key "leo"
-        let leo = await animals.set('leo', {
-            type:'cat',
-            color:'orange'
-        })
-
-        // get an item at key "leo" from collection animals
-        let item = await animals.get('leo')
 
         
-        res.send('Yo, yo')
+        res.send('Bof')
 
-    }
 
-    
-
-    run()
 
     
 })
