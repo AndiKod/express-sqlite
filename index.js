d4dfd02aaba0eefd9cf3a3946b7b979a58d56f26
AndiKod <andrei@andikod.fr>
2022-10-23 14:21:41 +0200

    foo

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo, yo, yo. Im a private Repo!')
+const CyclicDb = require("cyclic-dynamodb")
+const db = CyclicDb("brainy-shawl-elkCyclicDB")
+
+const animals = db.collection("animals")
+
+// create an item in collection with key "leo"
+let leo = await animals.set("leo", {
+    type: "cat",
+    color: "orange"
 })
+
+// get an item at key "leo" from collection animals
+let item = await animals.get("leo")
+
+app.all('/', (req, res) => {
+
+    
+
+    console.log("Just got a request!")
+    res.send('Yo ' + item.color + ' ' + item.type)
+})
+
 app.listen(process.env.PORT || 3000)
