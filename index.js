c16bbebeb2f0d4e0cb0cfc901f5e80a177ca11f8
AndiKod <andrei@andikod.fr>
2022-10-23 15:30:14 +0200

    bof3

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ const app = express()
 
 app.all('/', (req, res) => {
 
+    const CyclicDB = require('cyclic-dynamodb')
+    const db = CyclicDB('doubtful-dove-gownCyclicDB') 
+
     const run = async function(){
 
-    let foo = "bar"
+    let animals = db.collection('animals')
+
+    // create an item in collection with key "leo"
+    let leo = await animals.set('leo', {
+        type:'cat',
+        color:'orange'
+    })    
+
+       
+
+    let foo = " BAR "
     console.log("vraiment? aucun log?")
     res.send('Bof!' + foo)
 }
