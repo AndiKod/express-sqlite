2abd4d3ab27645e8d181b78ed895faf34225cdf0
AndiKod <andrei@andikod.fr>
2022-10-23 16:56:03 +0200

    colors

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const express = require('express')
 const app = express()
 
 
-
-
-
-
-
-
 app.all('/', (req, res) => {
+    res.send('De la merde')   
+})
 
+app.get('/color', (req, res) => {
 
+    //colors
+    let colors = [
+        'blue',
+        'purple',
+        'green',
+        'orange',
+        'yellow',
+        'red',
+        'navi'
+    ]
 
+    let randomColor = colors[Math.floor(Math.random() * colors.length)]
 
-    res.send('De la merde')
+    res.send(randomColor);
 
-
-
-    
 })
 
 app.listen(process.env.PORT || 3000)
