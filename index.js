4ef54fcd8ce3758208d75e2dd6dce88f3b3738c4
AndiKod <andrei@andikod.fr>
2022-10-24 23:06:27 +0200

    cors2

--- index.js
+++ index.js
@@ -55,12 +55,11 @@ app.get('/', (req, res) => res.render('index', {
 }));
 */
 
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-});
+ });
 
 
 
